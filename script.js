3bf243d801ef4ecc489a95bfa4d2330c43c87eb4
zaheer13068 <111473245+zaheer13068@users.noreply.github.com>
2024-01-17 20:39:26 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+
+    console.log('Ready')
+
+    //  Fetch the current date and update it in the DOM
+
+    let date = new Date()
+    let current_date = date.toDateString()
+
+
+    //  write an event, when Submit button is clicked
+    $('').click(function(){
+
+        //  get the text value from the textarea using the 'val()' method
+        let text_value = $('').val()
+
+        //  Convert it to JS object.
+        //  Provide a 'key' here and in write the same in app.py file as well to extract data
+        let input_text = {'' : text_value}
+        console.log(input_text)
+
+        //  ajax request
+        $.ajax({
+            
+
+            //  type of web request
+            type : '',
+
+            //  Data to be sent in JSON format
+            data : JSON.stringify(),
+
+            //  type of response expected is json
+            dataType : 'json',
+
+            //  contentType
+            contentType : 'application/json',
+
+            //  if everything is successful, run this function
+            success : function(result){
+
+                // extract prediction and emoticon url from result
+                
+
+                //  update the DOM elements
+
+
+                //  show them
+
+            },
+
+            //  if any error, run this function
+            error : function(result){
+
+                console.log(result)
+            }
+        })
+
+
+        //  clearing the textbox after every button push
+        $('#text').val("")
+    })
+        
+})
